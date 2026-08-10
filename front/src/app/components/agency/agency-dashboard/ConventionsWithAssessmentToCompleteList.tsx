09650682a8d2766f017737a71ba891a5f6388882
clement-duport <clement.duport@beta.gouv.fr>
2026-08-10 11:42:36 +0200

    Implement filtering and UI enhancements for unfinalized assessments in ConventionsWithAssessmentToCompleteList component

--- front/src/app/components/agency/agency-dashboard/ConventionsWithAssessmentToCompleteList.tsx
+++ front/src/app/components/agency/agency-dashboard/ConventionsWithAssessmentToCompleteList.tsx
@@ -2,10 +2,15 @@ import { fr } from "@codegouvfr/react-dsfr";
 import { Badge } from "@codegouvfr/react-dsfr/Badge";
 import Button from "@codegouvfr/react-dsfr/Button";
 import Pagination from "@codegouvfr/react-dsfr/Pagination";
-import { useCallback } from "react";
-import { HeadingSection, Task } from "react-design-system";
+import RadioButtons, {
+  type RadioButtonsProps,
+} from "@codegouvfr/react-dsfr/RadioButtons";
+import { equals } from "ramda";
+import { useCallback, useEffect, useMemo, useState } from "react";
+import { HeadingSection, RichDropdown, Task } from "react-design-system";
 import { useDispatch } from "react-redux";
 import {
+  type ConventionsWithUnfinalizedAssessmentFilters,
   type ConventionWithUnfinalizedAssessment,
   domElementIds,
   frontRoutes,
@@ -19,7 +24,10 @@ import {
 } from "src/app/utils/assessment.utils";
 import { authSelectors } from "src/core-logic/domain/auth/auth.selectors";
 import { connectedUserConventionsToManageSelectors } from "src/core-logic/domain/connected-user/conventionsToManage/connectedUserConventionsToManage.selectors";
-import { connectedUserConventionsToManageSlice } from "src/core-logic/domain/connected-user/conventionsToManage/connectedUserConventionsToManage.slice";
+import {
+  connectedUserConventionsToManageSlice,
+  initialConventionsWithUnfinalizedAssessmentFilters,
+} from "src/core-logic/domain/connected-user/conventionsToManage/connectedUserConventionsToManage.slice";
 
 export const ConventionsWithAssessmentToCompleteList = () => {
   const dispatch = useDispatch();
@@ -30,6 +38,81 @@ export const ConventionsWithAssessmentToCompleteList = () => {
   const pagination = useAppSelector(
     connectedUserConventionsToManageSelectors.conventionsWithUnfinalizedAssessmentPagination,
   );
+  const filters = useAppSelector(
+    connectedUserConventionsToManageSelectors.conventionsWithUnfinalizedAssessmentFilters,
+  );
+
+  const areFiltersEmpty = equals(
+    filters,
+    initialConventionsWithUnfinalizedAssessmentFilters,
+  );
+
+  const [tempFilters, setTempFilters] =
+    useState<ConventionsWithUnfinalizedAssessmentFilters>(() => ({
+      assessmentCompletionStatus: filters.assessmentCompletionStatus,
+    }));
+
+  useEffect(() => {
+    setTempFilters({
+      assessmentCompletionStatus: filters.assessmentCompletionStatus,
+    });
+  }, [filters.assessmentCompletionStatus]);
+
+  const assessmentOptions: RadioButtonsProps["options"] = useMemo(
+    () => [
+      {
+        label: getAssessmentLabelsAndSeverityByStatus({ isPlural: true })[
+          "to-complete"
+        ].longLabel,
+        nativeInputProps: {
+          value: "to-complete",
+          checked: tempFilters.assessmentCompletionStatus === "to-complete",
+          onChange: () => {
+            setTempFilters({
+              assessmentCompletionStatus: "to-complete",
+            });
+          },
+        },
+      },
+      {
+        label: getAssessmentLabelsAndSeverityByStatus({ isPlural: true })[
+          "to-sign"
+        ].longLabel,
+        nativeInputProps: {
+          value: "to-sign",
+          checked: tempFilters.assessmentCompletionStatus === "to-sign",
+          onChange: () => {
+            setTempFilters({
+              assessmentCompletionStatus: "to-sign",
+            });
+          },
+        },
+      },
+    ],
+    [tempFilters.assessmentCompletionStatus],
+  );
+
+  const onSubmit = useCallback(
+    (filtersToUse = tempFilters) => {
+      if (!connectedUserJwt) return;
+
+      dispatch(
+        connectedUserConventionsToManageSlice.actions.getConventionsWithUnfinalizedAssessmentRequested(
+          {
+            filters: {
+              page: 1,
+              perPage: NUMBER_ITEM_TO_DISPLAY_IN_PAGINATED_PAGE,
+              assessmentCompletionStatus:
+                filtersToUse.assessmentCompletionStatus,
+            },
+            jwt: connectedUserJwt,
+            feedbackTopic: "conventions-with-unfinalized-assessment",
+          },
+        ),
+      );
+    },
+    [connectedUserJwt, dispatch, tempFilters],
+  );
 
   const fetchConventions = useCallback(
     ({ page }: { page: number }) => {
@@ -38,6 +121,7 @@ export const ConventionsWithAssessmentToCompleteList = () => {
           connectedUserConventionsToManageSlice.actions.getConventionsWithUnfinalizedAssessmentRequested(
             {
               filters: {
+                ...filters,
                 page,
                 perPage: NUMBER_ITEM_TO_DISPLAY_IN_PAGINATED_PAGE,
               },
@@ -48,7 +132,7 @@ export const ConventionsWithAssessmentToCompleteList = () => {
         );
       }
     },
-    [connectedUserJwt, dispatch],
+    [connectedUserJwt, dispatch, filters],
   );
 
   const onPaginationClick = useCallback(
@@ -58,14 +142,76 @@ export const ConventionsWithAssessmentToCompleteList = () => {
     [fetchConventions],
   );
 
+  useEffect(() => {
+    if (connectedUserJwt) {
+      dispatch(
+        connectedUserConventionsToManageSlice.actions.getConventionsWithUnfinalizedAssessmentRequested(
+          {
+            filters: {
+              page: 1,
+              perPage: NUMBER_ITEM_TO_DISPLAY_IN_PAGINATED_PAGE,
+            },
+            jwt: connectedUserJwt,
+            feedbackTopic: "conventions-with-unfinalized-assessment",
+          },
+        ),
+      );
+    }
+
+    return () => {
+      dispatch(
+        connectedUserConventionsToManageSlice.actions.clearConventionsWithUnfinalizedAssessment(),
+      );
+    };
+  }, [dispatch, connectedUserJwt]);
+
+  const assessmentFilterTagValue = (() => {
+    if (tempFilters.assessmentCompletionStatus === "to-sign")
+      return `Bilan : ${getAssessmentLabelsAndSeverityByStatus({ isPlural: true })["to-sign"].longLabel}`;
+    if (tempFilters.assessmentCompletionStatus === "to-complete")
+      return `Bilan : ${getAssessmentLabelsAndSeverityByStatus({ isPlural: true })["to-complete"].longLabel}`;
+    return "Tous les bilans";
+  })();
+
   return (
     <HeadingSection
       title="Relances bilans"
       titleAs="h2"
       className={fr.cx("fr-mt-2w", "fr-mb-4w")}
     >
+      <form
+        onSubmit={(event: React.FormEvent<HTMLFormElement>) => {
+          event.preventDefault();
+          onSubmit();
+        }}
+        className={fr.cx("fr-grid-row", "fr-grid-row--middle", "fr-mb-4w")}
+      >
+        <RichDropdown
+          id="assessment-completion-status"
+          iconId="fr-icon-checkbox-line"
+          defaultValue="Tous les bilans"
+          className={fr.cx("fr-mr-2w")}
+          values={[assessmentFilterTagValue]}
+          submenu={{
+            title: "Filtrer par statut du bilan",
+            content: <RadioButtons options={assessmentOptions} />,
+          }}
+          onReset={() => {
+            const newFilters = {
+              assessmentCompletionStatus: undefined,
+            };
+            setTempFilters(newFilters);
+            onSubmit(newFilters);
+          }}
+          as="Tag"
+        />
+      </form>
       {conventions?.length === 0 && (
-        <p>Aucun bilan à compléter ou à signer pour le moment.</p>
+        <p>
+          {!areFiltersEmpty
+            ? "Aucun bilan trouvé avec ces filtres, vous pouvez modifier les filtres pour élargir votre recherche."
+            : "Aucun bilan à compléter ou à signer pour le moment."}
+        </p>
       )}
       {conventions.map((convention) => (
         <AssessmentToCompleteTaskItem
