9dc038583327cf24fa1ddd01c6c55f4413ce4b7e
Benjamin Bohec <bbohec.pro@gmail.com>
2026-08-12 13:49:59 +0200

    back - ArchivedConventionRequestQueries Pg integration
    Signed-off-by: Benjamin Bohec <bbohec.pro@gmail.com>

---
+++ back/src/domains/convention/adapters/archived-convention-request/ArchivedConventionRequestQueries.integration.test.ts
@@ -0,0 +1,130 @@
+import type { Pool } from "pg";
+import { ConnectedUserBuilder, executeInSequence, expectToEqual } from "shared";
+import { v4 as uuid } from "uuid";
+import {
+  type KyselyDb,
+  makeKyselyDb,
+} from "../../../../config/pg/kysely/kyselyUtils";
+import { makeTestPgPool } from "../../../../config/pg/pgPool";
+import { PgUserRepository } from "../../../core/authentication/connected-user/adapters/PgUserRepository";
+import type { ArchivedConventionRequestEntity } from "../../entities/ArchivedConventionRequestEntity";
+import type { FirstOldestArchivedConventionRequestToReviewList } from "../../ports/ArchivedConventionRequestQueries";
+import { InMemoryArchivedConventionRequestQueries } from "./InMemoryArchivedConventionRequestQueries";
+import { PgArchivedConventionRequestQueries } from "./PgArchivedConventionRequestQueries";
+import { PgArchivedConventionRequestRepository } from "./PgArchivedConventionRequestRepository";
+
+const adapters: ("InMemory" | "Pg")[] = ["Pg", "InMemory"];
+
+describe.each(adapters)("%s ArchivedConventionRequestQueries", (adapter) => {
+  const user = new ConnectedUserBuilder().withId(uuid()).buildUser();
+  let pool: Pool;
+  let db: KyselyDb;
+  let queries:
+    | PgArchivedConventionRequestQueries
+    | InMemoryArchivedConventionRequestQueries;
+
+  beforeAll(() => {
+    pool = makeTestPgPool();
+    db = makeKyselyDb(pool);
+  });
+
+  afterAll(async () => {
+    await pool.end();
+  });
+
+  beforeEach(async () => {
+    queries =
+      adapter === "Pg"
+        ? new PgArchivedConventionRequestQueries(db)
+        : new InMemoryArchivedConventionRequestQueries();
+
+    if (adapter === "Pg") {
+      await db.deleteFrom("archived_convention_requests").execute();
+      await db.deleteFrom("users").execute();
+      await new PgUserRepository(db).save(user);
+    }
+  });
+
+  describe("getFirstOldestArchivedConventionRequestToReviewList", () => {
+    it("get entities sorted by created at date", async () => {
+      const oldRequest: ArchivedConventionRequestEntity = {
+        id: uuid(),
+        userId: user.id,
+        createdAt: new Date("2022-01-01").toISOString(),
+        handledAt: null,
+        conventionSearchMethod: "withConventionId",
+        conventionId: uuid(),
+        reason: "legalDispute",
+      };
+
+      const newRequest: ArchivedConventionRequestEntity = {
+        id: uuid(),
+        userId: user.id,
+        createdAt: new Date("2026-01-01").toISOString(),
+        handledAt: null,
+        conventionSearchMethod: "withConventionId",
+        conventionId: uuid(),
+        reason: "legalDispute",
+      };
+
+      const expectedResults = [oldRequest, newRequest];
+
+      if (queries instanceof PgArchivedConventionRequestQueries) {
+        await saveEntitiesInRepo(db, expectedResults);
+      } else {
+        queries.getFirstOldestArchivedConventionRequestToReviewListNextResponse =
+          expectedResults.map(({ id, reason, userId, createdAt }) => ({
+            id,
+            reason,
+            userId,
+            createdAt,
+          }));
+      }
+
+      expectToEqual(
+        await queries.getFirstOldestArchivedConventionRequestToReviewList(),
+        expectedResults.map(({ id, reason, userId, createdAt }) => ({
+          id,
+          reason,
+          userId,
+          createdAt,
+        })),
+      );
+    });
+
+    it("do not get entities already handled", async () => {
+      const oldRequestAlreadyHandled: ArchivedConventionRequestEntity = {
+        id: uuid(),
+        userId: user.id,
+        createdAt: new Date("2022-01-01").toISOString(),
+        handledAt: new Date("2026-01-01").toISOString(),
+        conventionSearchMethod: "withConventionId",
+        conventionId: uuid(),
+        reason: "legalDispute",
+      };
+
+      const expectedResults: FirstOldestArchivedConventionRequestToReviewList =
+        [];
+
+      if (queries instanceof PgArchivedConventionRequestQueries) {
+        await saveEntitiesInRepo(db, [oldRequestAlreadyHandled]);
+      } else {
+        queries.getFirstOldestArchivedConventionRequestToReviewListNextResponse =
+          expectedResults;
+      }
+
+      expectToEqual(
+        await queries.getFirstOldestArchivedConventionRequestToReviewList(),
+        expectedResults,
+      );
+    });
+  });
+});
+
+async function saveEntitiesInRepo(
+  db: KyselyDb,
+  entities: ArchivedConventionRequestEntity[],
+) {
+  const repo = new PgArchivedConventionRequestRepository(db);
+  await executeInSequence(entities, (entity) => repo.save(entity));
+}
---
+++ back/src/domains/convention/adapters/archived-convention-request/PgArchivedConventionRequestQueries.ts
@@ -0,0 +1,30 @@
+import { archivedConventionRequestReasonSchema } from "shared";
+import type { KyselyDb } from "../../../../config/pg/kysely/kyselyUtils";
+import type {
+  ArchivedConventionRequestQueries,
+  FirstOldestArchivedConventionRequestToReviewList,
+} from "../../ports/ArchivedConventionRequestQueries";
+
+export class PgArchivedConventionRequestQueries
+  implements ArchivedConventionRequestQueries
+{
+  constructor(private readonly transaction: KyselyDb) {}
+
+  public async getFirstOldestArchivedConventionRequestToReviewList(): Promise<FirstOldestArchivedConventionRequestToReviewList> {
+    return this.transaction
+      .selectFrom("archived_convention_requests")
+      .select(["id", "reason", "user_id", "created_at"])
+      .where("handled_at", "is", null)
+      .orderBy("created_at", "asc")
+      .limit(100)
+      .execute()
+      .then((results) =>
+        results.map(({ id, reason, created_at, user_id: userId }) => ({
+          id,
+          reason: archivedConventionRequestReasonSchema.parse(reason),
+          userId,
+          createdAt: created_at.toISOString(),
+        })),
+      );
+  }
+}
