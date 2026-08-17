a89abb39a10afcce83dbe9ffd23e79367e16b76a
clement-duport <clement.duport@beta.gouv.fr>
2026-08-17 16:16:36 +0200

    Refactor ConventionReadPublicV2 DTO and schema to utilize PartnerAgencyKind, updating related tests for consistency.

--- back/src/adapters/primary/routers/DtoAndSchemas/v2/input/ConventionReadPublicV2.dto.ts
+++ back/src/adapters/primary/routers/DtoAndSchemas/v2/input/ConventionReadPublicV2.dto.ts
@@ -1,7 +1,31 @@
-import type { ConventionReadDto } from "shared";
+import type {
+  AgencyRefersToInConvention,
+  ConventionReadDto,
+  PartnerAgencyKind,
+} from "shared";
+import { toPartnerAgencyKind } from "../../../../../../utils/agency";
 
-export type ConventionReadPublicV2Dto = ConventionReadDto;
+export type ConventionReadPublicV2Dto = Omit<
+  ConventionReadDto,
+  "agencyKind" | "agencyRefersTo"
+> & {
+  agencyKind: PartnerAgencyKind;
+  agencyRefersTo?: Omit<AgencyRefersToInConvention, "kind"> & {
+    kind: PartnerAgencyKind;
+  };
+};
 
 export const conventionReadToConventionReadPublicV2 = (
   conventionReadDto: ConventionReadDto,
-): ConventionReadPublicV2Dto => conventionReadDto;
+): ConventionReadPublicV2Dto => ({
+  ...conventionReadDto,
+  agencyKind: toPartnerAgencyKind(conventionReadDto.agencyKind),
+  ...(conventionReadDto.agencyRefersTo
+    ? {
+        agencyRefersTo: {
+          ...conventionReadDto.agencyRefersTo,
+          kind: toPartnerAgencyKind(conventionReadDto.agencyRefersTo.kind),
+        },
+      }
+    : {}),
+});
