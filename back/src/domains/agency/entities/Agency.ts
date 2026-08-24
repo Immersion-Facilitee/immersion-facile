dcd9a3c7e2631257db232a064aed0133a227cd65
clement-duport <clement.duport@beta.gouv.fr>
2026-08-24 13:49:10 +0200

    Refactor AddAgency use case to allow creation of agencies with the same address and kind as existing ones.

--- back/src/domains/agency/entities/Agency.ts
+++ back/src/domains/agency/entities/Agency.ts
@@ -1,33 +1,10 @@
 import { toPairs } from "ramda";
-import {
-  type AgencyRole,
-  type AgencyUsersRights,
-  type AgencyWithUsersRights,
-  ConflictError,
-  type ConnectedUser,
-  type UserId,
+import type {
+  AgencyRole,
+  AgencyUsersRights,
+  ConnectedUser,
+  UserId,
 } from "shared";
-import type { UnitOfWork } from "../../core/unit-of-work/ports/UnitOfWork";
-
-export const throwConflictErrorOnSimilarAgencyFound = async ({
-  uow,
-  agency,
-}: {
-  uow: UnitOfWork;
-  agency: AgencyWithUsersRights;
-}) => {
-  const hasSimilarAgency =
-    await uow.agencyRepository.alreadyHasActiveAgencyWithSameAddressAndKind({
-      address: agency.address,
-      kind: agency.kind,
-      idToIgnore: agency.id,
-    });
-
-  if (hasSimilarAgency)
-    throw new ConflictError(
-      "Une autre agence du même type existe avec la même adresse",
-    );
-};
 
 export const getUserIdsWithoutRoleFromAgencyRights = ({
   rights,
