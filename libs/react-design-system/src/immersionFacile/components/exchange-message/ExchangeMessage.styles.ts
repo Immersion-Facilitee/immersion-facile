a001d8df7203ac3eb6780a367d3fd1db4cea489b
tpicaud <tom.picaud@hotmail.com>
2026-08-26 12:18:12 +0200

    richer ExchangeMessage custom component and adapt for mobile

--- libs/react-design-system/src/immersionFacile/components/exchange-message/ExchangeMessage.styles.ts
+++ libs/react-design-system/src/immersionFacile/components/exchange-message/ExchangeMessage.styles.ts
@@ -6,4 +6,7 @@ export default {
   establishment: "im-exchange-message--establishment",
   forEstablishment: "im-exchange-message--for-establishment",
   forPotentialBeneficiary: "im-exchange-message--for-potential-beneficiary",
+  headerMeta: "im-exchange-message__header-meta",
+  badge: "im-exchange-message__badge",
+  title: "im-exchange-message__title",
 };
