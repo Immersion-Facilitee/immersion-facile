d2be7ff19455beffd3aabf57e57ecbc2486c01e1
celineung <celine.ung.pro@gmail.com>
2026-09-07 10:23:01 +0200

    fix index.ts exports

--- libs/html-templates/src/components/email/index.ts
+++ libs/html-templates/src/components/email/index.ts
@@ -1,8 +1,8 @@
-export * from "./button";
-export * from "./content";
-export * from "./footer";
-export * from "./greetings";
-export * from "./head";
-export * from "./header";
-export * from "./highlight";
-export * from "./legals";
+export { type EmailButtonProps, renderButton } from "./button";
+export { renderContent } from "./content";
+export { defaultEmailFooter, renderFooter } from "./footer";
+export { renderGreetings } from "./greetings";
+export { renderHead } from "./head";
+export { cciCustomHtmlHeader, renderHeader } from "./header";
+export { renderHighlight } from "./highlight";
+export { renderLegals } from "./legals";
