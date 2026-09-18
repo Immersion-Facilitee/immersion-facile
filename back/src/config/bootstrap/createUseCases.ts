384f625a8d2137464f8e4b04f05cf7755de37c9c
celineung <celine.ung.pro@gmail.com>
2026-09-18 10:33:11 +0200

    use magicLink for SMS in SendSignatureLink

--- back/src/config/bootstrap/createUseCases.ts
+++ back/src/config/bootstrap/createUseCases.ts
@@ -1075,6 +1075,7 @@ export const createUseCases = ({
         timeGateway,
         config,
         saveNotificationAndRelatedEvent,
+        generateConventionMagicLinkUrl,
         shortLinkIdGeneratorGateway: gateways.shortLinkGenerator,
         createNewEvent,
       },
