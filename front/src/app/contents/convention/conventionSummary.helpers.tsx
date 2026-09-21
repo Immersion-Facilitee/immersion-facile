450a5993f96611b1bfa09060852c76cab3e08704
Enguerran Weiss <enguerranweiss@gmail.com>
2026-09-21 15:00:10 +0200

    biome fixes direct arrow function / block function / class and objects  methods returns

--- front/src/app/contents/convention/conventionSummary.helpers.tsx
+++ front/src/app/contents/convention/conventionSummary.helpers.tsx
@@ -545,7 +545,7 @@ const makeBeneficiarySubSections = (
           ),
         }),
       },
-{
+      {
         key: "beneficiaryRqth",
         label: "RQTH",
         value: convention.signatories.beneficiary.isRqth ? "Oui" : "Non",
@@ -623,7 +623,6 @@ const makeBeneficiarySubSections = (
     ],
   },
 ];
-
 
 const makeEstablishmentSubSections = (
   convention: ConventionReadDto,
