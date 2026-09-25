7f5524fd13dfc08e8bb57ae94639eaa2b4fa6328
Enguerran Weiss <enguerranweiss@gmail.com>
2026-09-25 14:54:47 +0200

    added actions for discussion mini-stage

--- front/src/app/components/discussion/exchanges/DiscussionExchangesList.tsx
+++ front/src/app/components/discussion/exchanges/DiscussionExchangesList.tsx
@@ -23,43 +23,41 @@ export const DiscussionExchangesList = ({
   sortedExchanges: ExchangeRead[];
   potentialBeneficiary: DiscussionReadDto["potentialBeneficiary"];
   viewer: ExchangeRole;
-}): JSX.Element => {
-  return (
-    <BorderedSection className={fr.cx("fr-mt-2w")}>
-      {sortedExchanges.map((exchange) => {
-        const currentMessage = addLineBreakOnNewLines(
-          convertHtmlToText(exchange.message),
-        );
-        const messageToDisplay = splitTextOnFirstSeparator(
-          currentMessage,
-          emailExchangeSplitters,
-        );
-        return (
-          <ExchangeMessage
-            key={exchange.sentAt}
-            viewer={viewer}
-            sender={exchange.sender}
-            title={
-              exchange.sender === "establishment"
-                ? `${exchange.firstname} ${exchange.lastname}`
-                : getFormattedFirstnameAndLastname({
-                    firstname: potentialBeneficiary.firstName,
-                    lastname: potentialBeneficiary.lastName,
-                  })
-            }
-            sentAt={toDisplayedDate({
-              date: new Date(exchange.sentAt),
-              withHours: true,
-            })}
-          >
-            <div
-              dangerouslySetInnerHTML={{
-                __html: DOMPurify.sanitize(messageToDisplay[0]),
-              }}
-            />
-          </ExchangeMessage>
-        );
-      })}
-    </BorderedSection>
-  );
-};
+}): JSX.Element => (
+  <BorderedSection className={fr.cx("fr-mt-2w")}>
+    {sortedExchanges.map((exchange) => {
+      const currentMessage = addLineBreakOnNewLines(
+        convertHtmlToText(exchange.message),
+      );
+      const messageToDisplay = splitTextOnFirstSeparator(
+        currentMessage,
+        emailExchangeSplitters,
+      );
+      return (
+        <ExchangeMessage
+          key={exchange.sentAt}
+          viewer={viewer}
+          sender={exchange.sender}
+          title={
+            exchange.sender === "establishment"
+              ? `${exchange.firstname} ${exchange.lastname}`
+              : getFormattedFirstnameAndLastname({
+                  firstname: potentialBeneficiary.firstName,
+                  lastname: potentialBeneficiary.lastName,
+                })
+          }
+          sentAt={toDisplayedDate({
+            date: new Date(exchange.sentAt),
+            withHours: true,
+          })}
+        >
+          <div
+            dangerouslySetInnerHTML={{
+              __html: DOMPurify.sanitize(messageToDisplay[0]),
+            }}
+          />
+        </ExchangeMessage>
+      );
+    })}
+  </BorderedSection>
+);
